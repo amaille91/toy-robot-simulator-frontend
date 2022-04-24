cca739b85fd0a6e9be1bcd48fdeac691d92ec307
Alexandre Maille <an email>
2022-04-24 15:32:12 +0200

    Binding with update of properties

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -2,7 +2,7 @@
 export default {
   data() {
     return {
-      linePlaceValue: undefined,
+      linePlaceValue: 0,
       columnPlaceValue: undefined,
       orientationPlaceValue: undefined
     }
@@ -12,7 +12,7 @@ export default {
   },
   methods: {
     handlePlaceClick(event) {
-      console.log("Place button clicked");
+      console.log(`Place button clicked with line ${this.linePlaceValue}, column ${this.columnPlaceValue} and orientation ${this.orientationPlaceValue}`);
     },
     handleMoveClick(event) {
       console.log("Move button clicked");
@@ -32,22 +32,22 @@ export default {
 
 <template>
     <div class="container">
-      <div class="row">
+      <div class="row align-items-end">
         <div class="col" id="place-action-container">
           <div class="row pb-2">
             <div class="col-4">
               <label for="line-select" class="row mx-auto p-0">line:</label>
-              <input type="number" placeholder="0-4" name="line-select" class="row w-100 mx-auto">
+              <input type="number" :value="linePlaceValue" @input="event => linePlaceValue = event.target.value" placeholder="0-4" name="line-select" class="row w-100 mx-auto">
             </div>
 
             <div class="col-4">
               <label for="col-select" class="row mx-auto p-0">column:</label>
-              <input type="number" placeholder="0-4" name="col-select" class="row w-100 mx-auto">
+              <input type="number" v-model="columnPlaceValue" placeholder="0-4" name="col-select" class="row w-100 mx-auto">
             </div>
 
             <div class="col-4">
               <label for="orientation-select" class="row mx-auto p-0">NESW</label>
-              <select name="orientation-select" class="row w-100 mx-auto">
+              <select name="orientation-select" v-model="orientationPlaceValue" class="row w-100 mx-auto">
                 <option value="N">NORTH</option>
                 <option value="E">EAST</option>
                 <option value="S">SOUTH</option>
@@ -57,24 +57,24 @@ export default {
           </div>
 
           <div class="row">
-            <button @click="console.log('Place button has been clicked')">Place</button>
+            <button class="w-100" @click="handlePlaceClick()">Place</button>
           </div>
         </div>
 
         <div class="col" id="move-action-container">
-            <button @click="console.log('Move button has been clicked')">Move</button>
+            <button class="w-100" @click="handleMoveClick()">Move</button>
         </div>
 
         <div class="col" id="left-action-container">
-            <button @click="console.log('Left button has been clicked')">Left</button>
+            <button class="w-100" @click="handleLeftClick()">Left</button>
         </div>
 
         <div class="col" id="right-action-container">
-            <button @click="console.log('Right button has been clicked')">Right</button>
+            <button class="w-100" @click="handleRightClick()">Right</button>
         </div>
 
         <div class="col" id="report-action-container">
-            <button @click="console.log('Report button has been clicked')">Report</button>
+            <button class="w-100" @click="handleReportClick()">Report</button>
         </div>
       </div>
     </div>
