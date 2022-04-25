a311b004665c201feced911f5dc679d038779c46
Alexandre Maille <al.maille@laposte.net>
2022-04-25 18:52:07 +0200

    peaufinage du style

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,7 @@ import RobotGrid from './components/RobotGrid.vue'
 </script>
 
 <template>
-  <header>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  </header>
-
-  <main>
+  <main class="p-4">
     <RobotGrid />
     <Report />
     <Controls />
