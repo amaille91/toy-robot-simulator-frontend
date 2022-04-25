ff58ed23545b322a9d6a0889171cc6cf5352ebcc
Alexandre Maille <al.maille@laposte.net>
2022-04-25 18:48:45 +0200

    final integration test

--- src/components/__tests__/App.spec.js
+++ src/components/__tests__/App.spec.js
@@ -38,4 +38,25 @@ describe("App", () => {
     await wrapper.find('#report-button').trigger('click');
     expect(wrapper.find('#report-message').text()).toEqual('Report: Robot is at (0, 0) and is facing W')
   });
+
+  it("should be at (3, 3) when going move move left move from (2, 1) facing east", async () => {
+    const wrapper = mount(App);
+    expect(wrapper.find('.container-fluid').exists()).toBe(true);
+    await wrapper.find('#line-input').setValue(2);
+    await wrapper.find('input[name="col-input"]').setValue(1);
+    await wrapper.find('select[name="orientation-select"]').setValue('E');
+
+    await wrapper.find('#place-button').trigger('click');
+    expect(wrapper.find('#grid-place-2-1 #svg-east').exists()).toBe(true);
+
+    await wrapper.find('#move-button').trigger('click');
+    await wrapper.find('#move-button').trigger('click');
+    await wrapper.find('#left-button').trigger('click');
+    await wrapper.find('#move-button').trigger('click');
+
+    expect(wrapper.find('#grid-place-3-3 #svg-north').exists()).toBe(true);
+
+    await wrapper.find('#report-button').trigger('click');
+    expect(wrapper.find('#report-message').text()).toEqual('Report: Robot is at (3, 3) and is facing N')
+  });
 });
