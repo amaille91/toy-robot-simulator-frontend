9660369d5f7024374d6cde7b1b140d311fb401cb
Alexandre Maille <al.maille@laposte.net>
2022-04-25 18:18:32 +0200

    Le robot bouge!

--- src/components/RobotGrid.vue
+++ src/components/RobotGrid.vue
@@ -1,40 +1,108 @@
 <script lang="ts">
+import RobotRepresentation from './RobotRepresentation.vue'
 import {controller} from './controller.js'
 export default {
+  components: {
+    RobotRepresentation
+  },
+  data() {
+    return {
+      controller
+    }
+  },
+  methods: {
+    isRobotPlacedAt(line, col) {
+      return controller.robotState &&
+        controller.robotState.position.line === line &&
+        controller.robotState.position.col === col
+    }
+  }
 }
 </script>
 
 <template>
     <div class="robot-grid m-auto">
-      <div class="grid-place w-100"></div>
-      <div class="grid-place"></div>
-      <div class="grid-place"></div>
-      <div class="grid-place"></div>
-      <div class="grid-place"></div>
-      
-      <div class="grid-place"></div>
-      <div class="grid-place"></div>
-      <div class="grid-place"></div>
-      <div class="grid-place"></div>
-      <div class="grid-place"></div>
 
-      <div class="grid-place"></div>
-      <div class="grid-place"></div>
-      <div class="grid-place"></div>
-      <div class="grid-place"></div>
-      <div class="grid-place"></div>
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(4,0)" :orientation="controller.robotState.orientation" />
+      </div>
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(4,1)" :orientation="controller.robotState.orientation" />
+      </div>
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(4,2)" :orientation="controller.robotState.orientation" />
+      </div>
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(4,3)" :orientation="controller.robotState.orientation" />
+      </div>
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(4,4)" :orientation="controller.robotState.orientation" />
+      </div>
+
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(3,0)" :orientation="controller.robotState.orientation" />
+      </div>
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(3,1)" :orientation="controller.robotState.orientation" />
+      </div>
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(3,2)" :orientation="controller.robotState.orientation" />
+      </div>
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(3,3)" :orientation="controller.robotState.orientation" />
+      </div>
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(3,4)" :orientation="controller.robotState.orientation" />
+      </div>
+
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(2,0)" :orientation="controller.robotState.orientation" />
+      </div>
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(2,1)" :orientation="controller.robotState.orientation" />
+      </div>
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(2,2)" :orientation="controller.robotState.orientation" />
+      </div>
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(2,3)" :orientation="controller.robotState.orientation" />
+      </div>
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(2,4)" :orientation="controller.robotState.orientation" />
+      </div>
+
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(1,0)" :orientation="controller.robotState.orientation" />
+      </div>
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(1,1)" :orientation="controller.robotState.orientation" />
+      </div>
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(1,2)" :orientation="controller.robotState.orientation" />
+      </div>
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(1,3)" :orientation="controller.robotState.orientation" />
+      </div>
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(1,4)" :orientation="controller.robotState.orientation" />
+      </div>
+
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(0,0)" :orientation="controller.robotState.orientation" />
+      </div>
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(0,1)" :orientation="controller.robotState.orientation" />
+      </div>
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(0,2)" :orientation="controller.robotState.orientation" />
+      </div>
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(0,3)" :orientation="controller.robotState.orientation" />
+      </div>
+      <div class="grid-place">
+        <RobotRepresentation v-if="this.isRobotPlacedAt(0,4)" :orientation="controller.robotState.orientation" />
+      </div>
       
-      <div class="grid-place"></div>
-      <div class="grid-place"></div>
-      <div class="grid-place"></div>
-      <div class="grid-place"></div>
-      <div class="grid-place"></div>
-      
-      <div class="grid-place"></div>
-      <div class="grid-place"></div>
-      <div class="grid-place"></div>
-      <div class="grid-place"></div>
-      <div class="grid-place"></div>
     </div>
 </template>
 
